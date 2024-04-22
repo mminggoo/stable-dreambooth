001db0abb11c3fdac50e42a5191466da4d53aebd
mminggoo <121415822+mminggoo@users.noreply.github.com>
2024-04-22 15:43:01 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 from diffusers.pipelines import StableDiffusionPipeline
 import torch
+import os
 
 import argparse
 
@@ -18,6 +19,8 @@ save_dir = args.save_dir
 
 
 if __name__ == "__main__":
+    os.makedirs(save_dir, exist_ok=True)
+    
     model_id = args.model
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = StableDiffusionPipeline.from_pretrained(model_id).to(device)
